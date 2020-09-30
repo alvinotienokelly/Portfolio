b98eda910acada09ebe7e283a7d4db36436720e1
otienoalvin44@gmail.com <otienoalvin44@gmail.com>
2020-09-30 05:21:41 +0300

    vue particles

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="" style="background-color: #001a33" >
     <vue-particles style="z-index: -1"
-        color="#ffffff"
-        :particleOpacity="0.6"
-        :particlesNumber="80"
-        shapeType="circle"
-        :particleSize="4"
-        linesColor="#ffffff"
-        :linesWidth="1"
-        :lineLinked="true"
-        :lineOpacity="0.0"
-        :linesDistance="150"
-        :moveSpeed="3"
-        :hoverEffect="true"
-        hoverMode="repulse"
-        :clickEffect="true"
-        clickMode="repulse"
+                   color="#ffffff"
+                   :particleOpacity="0.6"
+                   :particlesNumber="80"
+                   shapeType="circle"
+                   :particleSize="4"
+                   linesColor="#ffffff"
+                   :linesWidth="1"
+                   :lineLinked="true"
+                   :lineOpacity="0.0"
+                   :linesDistance="150"
+                   :moveSpeed="3"
+                   :hoverEffect="true"
+                   hoverMode="repulse"
+                   :clickEffect="true"
+                   clickMode="repulse"
     >
     </vue-particles>
     <services></services>
@@ -23,10 +23,8 @@
 </template>
 
 <script>
-import Services from "@/components/services";
 export default {
   name: 'HelloWorld',
-  components: {Services},
   props: {
     msg: String
   }
