46b98059dddfaa00e7f37b6365505de985eadced
otienoalvin44@gmail.com <otienoalvin44@gmail.com>
2020-10-01 07:01:57 +0300

    work age changes

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -1,11 +1,43 @@
 <template>
-  <div>
+  <div class="">
     <div class="jumbotron jumbotron-fluid">
-      <div class="container">
-        <h1 class="display-4">My Work</h1>
-        <p class="lead">I am a Fullstack Software Engineer with a keen eye in design.</p>
-      </div>
+
     </div>
+    <div class="row col-lg-12 col-md-12" style="margin-top: 100px;margin-bottom: 20px">
+      <div class="col-lg-6 col-md-6">
+        <img class="card-img-top" src="../assets/sk.png"
+             style="width: 30rem" alt="Card image cap">
+
+      </div>
+      <div class="col-lg-6 col-md-6 text-justify" style="margin-top: 70px">
+        <h4 style="font-weight: bolder">Savagink Kenya</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean consectetur,
+          leo ac blandit laoreet, turpis neque  convallis metus, eget suscipit nisl magna
+          id tortor. Etiam nec ultrices diam</p>
+        <div class="visit" style="margin-top: 20px">
+          <button class="btn-primary btn btn-lg "  style="font-weight: bolder">Visit</button>
+        </div>
+      </div>
+
+    </div>
+    <div class="row col-lg-12 col-md-12"  style="margin-top: 100px;margin-bottom: 20px">
+      <div class="col-lg-6 col-md-6">
+        <img class="card-img-top" src="../assets/me.png"
+             style="width: 30rem" alt="Card image cap">
+
+      </div>
+      <div class="col-lg-6 col-md-6 text-justify" style="margin-top: 70px">
+        <h4 style="font-weight: bolder">My portfolio site</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean consectetur,
+          leo ac blandit laoreet, turpis neque  convallis metus, eget suscipit nisl magna
+          id tortor. Etiam nec ultrices diam</p>
+        <div class="visit" style="margin-top: 20px">
+          <button class="btn-primary btn "  style="font-weight: bolder">Visit</button>
+        </div>
+      </div>
+
+    </div>
+
     <div>
 
     </div>
